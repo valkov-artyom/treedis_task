fc582095ceb870c9d68b8973a963940c549e756a
Artem Valkov <valkov.artyom@gmail.com>
2020-11-19 07:55:45 +0200

    fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "./Login";
 import Chat from './Chat'
 
